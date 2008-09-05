de5a7e407bc43de64c37a25012754845d7454713
ahoerstemeier <ahoerstemeier@gmail.com>
2008-09-05 21:48:59 +0000

    TambonCreationStatistics - Muban numbers

--- TambonCreationStatistics.cs
+++ TambonCreationStatistics.cs
@@ -7,6 +7,7 @@ namespace De.AHoerstemeier.Tambon
 {
     class TambonCreationStatistics
     {
+        const Int32 MAXIMUMMUBAN = 50;
         #region properties
         public Int32 StartYear { get; set; }
         public Int32 EndYear { get; set; }
@@ -14,8 +15,7 @@ namespace De.AHoerstemeier.Tambon
         public Int32 NumberOfAnnouncements { get { return mNumberOfAnnouncements; } }
         private Int32 mNumberOfTambonCreations;
         public Int32 NumberOfTambonCreations { get { return mNumberOfTambonCreations; } }
-        private Dictionary<Int32, List<Int32>> mNumberOfMuban = new Dictionary<int,List<int>>();
-        public Dictionary<Int32, List<Int32>> NumberOfMuban { get { return mNumberOfMuban; } }
+        private List<Int32>[] mNumberOfMuban = new List<Int32>[MAXIMUMMUBAN];
         private Int32[] mNumberOfTambonCreationsPerChangwat = new Int32[100];
         #endregion
         #region constructor
@@ -32,8 +32,8 @@ namespace De.AHoerstemeier.Tambon
         #endregion
         #region methods
         public void Calculate()
-        { 
-            mNumberOfMuban = new Dictionary<int,List<int>>();
+        {
+            mNumberOfMuban = new List<Int32>[MAXIMUMMUBAN];
             mNumberOfTambonCreationsPerChangwat = new Int32[100];
             mNumberOfTambonCreations = 0;
             mNumberOfAnnouncements = 0;
@@ -50,7 +50,7 @@ namespace De.AHoerstemeier.Tambon
                             RoyalGazetteContentCreate lCreate = (RoyalGazetteContentCreate)lContent;
                             if (lCreate.Status == EntityType.Tambon)
                             {
-                                lfound = true;                                
+                                lfound = true;
                                 ProcessCreation(lCreate);
                             }
                         }
@@ -63,18 +63,32 @@ namespace De.AHoerstemeier.Tambon
             }
         }
 
-        private void ProcessCreation(RoyalGazetteContentCreate lCreate)
+        private void ProcessCreation(RoyalGazetteContentCreate iCreate)
         {
             mNumberOfTambonCreations++;
 
-            Int32 lGeocode = lCreate.Geocode;
+            Int32 lGeocode = iCreate.Geocode;
             while (lGeocode > 100)
             {
                 lGeocode = lGeocode / 100;
             }
             mNumberOfTambonCreationsPerChangwat[lGeocode]++;
 
-            // ToDo: Count Muban and fill list
+            Int32 lMaxMubanIndex = 0;
+            foreach (RoyalGazetteContent lSubEntry in iCreate.SubEntities)
+            {
+                if (lSubEntry is RoyalGazetteContentReassign)
+                {
+                    Int32 lMubanCode = lSubEntry.Geocode % 100;
+                    lMaxMubanIndex = Math.Max(lMaxMubanIndex, lMubanCode);
+                }
+            }
+
+            if (mNumberOfMuban[lMaxMubanIndex] == null)
+            {
+                mNumberOfMuban[lMaxMubanIndex] = new List<Int32>();
+            }
+            mNumberOfMuban[lMaxMubanIndex].Add(iCreate.Geocode);
         }
         #endregion
 
@@ -106,8 +120,68 @@ namespace De.AHoerstemeier.Tambon
                     lBuilder.AppendLine("* " + lName);
                 }
                 lBuilder.AppendLine();
-            
-            // ToDo: The village data
+
+                Int32 lMostCommonNumber = 0;
+                Int32 lMostCommonNumberValue = 0;
+                Int32 lHighestNumber = 0;
+                Int32 lLowestNumber = MAXIMUMMUBAN;
+                Int32 lCount = 0;
+                Int32 lSum = 0;
+
+                for (int i = 1; i < MAXIMUMMUBAN; i++)
+                {
+                    if (mNumberOfMuban[i] != null)
+                    {
+                        Int32 lNumber = mNumberOfMuban[i].Count;
+                        if (lNumber > 0)
+                        {
+                            lCount = lCount + lNumber;
+                            lSum = lSum + lNumber * i;
+                            lHighestNumber = i;
+                            if (i < lLowestNumber)
+                            {
+                                lLowestNumber = i;
+                            }
+                            if (lNumber > lMostCommonNumberValue)
+                            {
+                                lMostCommonNumber = i;
+                                lMostCommonNumberValue = lNumber;
+                            }
+                        }
+                    }
+                }
+                if (lCount > 0)
+                {
+                    double lDeviation = 0;
+                    double lMeanValue = (lSum * 1.0 / lCount);
+                    for (int i = 1; i < MAXIMUMMUBAN; i++)
+                    {
+                        if (mNumberOfMuban[i] != null)
+                        {
+                            Int32 lNumber = mNumberOfMuban[i].Count;
+                            if (lNumber > 0)
+                            {
+                                lDeviation = lDeviation + Math.Pow(i - lMeanValue, 2);
+                            }
+                        }
+                    }
+                    lDeviation = Math.Sqrt(lDeviation / lCount);
+                    lBuilder.AppendLine("Most common number of muban: " + lMostCommonNumber.ToString() + " (" + lMostCommonNumberValue.ToString() + " times)");
+                    lBuilder.AppendLine("Mean number of muban: " + lMeanValue.ToString());
+                    lBuilder.AppendLine("Standard deviation: " + lDeviation.ToString());
+                    lBuilder.AppendLine("Lowest number of muban: " + lLowestNumber.ToString());
+                    foreach (Int32 lGeocode in mNumberOfMuban[lLowestNumber])
+                    {
+                        lBuilder.Append(lGeocode.ToString() + ' ');
+                    }
+                    lBuilder.AppendLine();
+                    lBuilder.AppendLine("Highest number of muban: " + lHighestNumber.ToString());
+                    foreach (Int32 lGeocode in mNumberOfMuban[lHighestNumber])
+                    {
+                        lBuilder.Append(lGeocode.ToString() + ' ');
+                    }
+                    lBuilder.AppendLine();
+                }
             }
 
             String retval = lBuilder.ToString();
