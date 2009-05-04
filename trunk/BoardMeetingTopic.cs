8d51d31a5df8e599c8eff1b7b8a965f2b67671d9
ahoerstemeier <ahoerstemeier@1281d315-5448-0410-8fcd-5bcd7f7bbbc7>
2009-05-04 22:03:03 +0000

    Statistics of time between board meeting and publication

---
+++ trunk/BoardMeetingTopic.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace De.AHoerstemeier.Tambon
+{
+    class BoardMeetingTopic
+    {
+        public DateTime Effective { get; set; }
+        public RoyalGazette Gazette { get; set; }
+        public RoyalGazetteContent Topic { get; set; }
+
+        public TimeSpan TimeTillPublish()
+        {
+            TimeSpan RetVal = new TimeSpan(0);
+            if (Gazette != null)
+            {
+                RetVal = Gazette.Publication - Effective;
+            }
+            return RetVal;
+        }
+        public void FindGazette()
+        {
+            foreach (RoyalGazette lGazette in Helper.GlobalGazetteList)
+            {
+                foreach (RoyalGazetteContent lGazetteContent in lGazette.Content)
+                {
+                    Boolean lFitting = false;
+                    if (Topic.GetType() == typeof(RoyalGazetteContentStatus))
+                    {
+                        lFitting = (lGazetteContent.GetType() == typeof(RoyalGazetteContentConstituency));
+                    }
+//                    if (this.GetType() == typeof(RoyalGazetteContentRename))
+//                    {
+//                        lFitting = (lGazetteContent.GetType() == typeof(RoyalGazetteContentRename));
+//                    }
+                    if (lFitting)
+                    {
+                        if (Topic.Geocode != 0)
+                        {
+                            if (lGazetteContent.IsAboutGeocode(Topic.Geocode, false))
+                            {
+                                Gazette = lGazette;
+                            }
+                        }
+                        if (Topic.TambonGeocode != 0)
+                        {
+                            if (lGazetteContent.IsAboutGeocode(Topic.TambonGeocode, false))
+                            {
+                                Gazette = lGazette;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
