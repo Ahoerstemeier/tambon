6af04a53daa6799f54a33cc96f18c7b848aafa54
ahoerstemeier <ahoerstemeier@1281d315-5448-0410-8fcd-5bcd7f7bbbc7>
2009-05-22 15:57:47 +0000

    Gazette entries with "mention" now also processed

---
+++ trunk/RoyalGazetteContentMention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace De.AHoerstemeier.Tambon
+{
+    class RoyalGazetteContentMention:RoyalGazetteContent
+    {
+        internal const String XmlLabel = "mention";
+        protected override String GetXmlLabel()
+        {
+            return XmlLabel;
+        }
+    }
+}
