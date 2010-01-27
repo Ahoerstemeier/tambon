489729cfd311747670736bc17cf4242d02a170d5
ahoerstemeier <ahoerstemeier@gmail.com>
2010-01-27 23:34:38 +0000

    GeoDatum selection in coordinate transform dialog

--- GeoCoordinateForm.Designer.cs
+++ GeoCoordinateForm.Designer.cs
@@ -34,6 +34,8 @@
             this.edit_UTM = new System.Windows.Forms.TextBox();
             this.lbl_LatLong = new System.Windows.Forms.Label();
             this.edit_LatLong = new System.Windows.Forms.TextBox();
+            this.cbx_datum = new System.Windows.Forms.ComboBox();
+            this.lbl_datum = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbl_MGRS
@@ -47,6 +49,8 @@
             // 
             // edit_MGRS
             // 
+            this.edit_MGRS.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.edit_MGRS.Location = new System.Drawing.Point(94, 6);
             this.edit_MGRS.Name = "edit_MGRS";
             this.edit_MGRS.Size = new System.Drawing.Size(186, 20);
@@ -64,6 +68,8 @@
             // 
             // edit_UTM
             // 
+            this.edit_UTM.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.edit_UTM.Location = new System.Drawing.Point(94, 31);
             this.edit_UTM.Name = "edit_UTM";
             this.edit_UTM.Size = new System.Drawing.Size(186, 20);
@@ -80,16 +86,41 @@
             // 
             // edit_LatLong
             // 
+            this.edit_LatLong.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.edit_LatLong.Location = new System.Drawing.Point(94, 57);
             this.edit_LatLong.Name = "edit_LatLong";
             this.edit_LatLong.Size = new System.Drawing.Size(186, 20);
             this.edit_LatLong.TabIndex = 5;
+            // 
+            // cbx_datum
+            // 
+            this.cbx_datum.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbx_datum.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_datum.FormattingEnabled = true;
+            this.cbx_datum.Location = new System.Drawing.Point(94, 83);
+            this.cbx_datum.Name = "cbx_datum";
+            this.cbx_datum.Size = new System.Drawing.Size(186, 21);
+            this.cbx_datum.TabIndex = 6;
+            this.cbx_datum.SelectedIndexChanged += new System.EventHandler(this.edit_MGRS_TextChanged);
+            // 
+            // lbl_datum
+            // 
+            this.lbl_datum.AutoSize = true;
+            this.lbl_datum.Location = new System.Drawing.Point(12, 86);
+            this.lbl_datum.Name = "lbl_datum";
+            this.lbl_datum.Size = new System.Drawing.Size(38, 13);
+            this.lbl_datum.TabIndex = 7;
+            this.lbl_datum.Text = "Datum";
             // 
             // GeoCoordinateForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.lbl_datum);
+            this.Controls.Add(this.cbx_datum);
             this.Controls.Add(this.edit_LatLong);
             this.Controls.Add(this.lbl_LatLong);
             this.Controls.Add(this.edit_UTM);
@@ -98,6 +129,7 @@
             this.Controls.Add(this.lbl_MGRS);
             this.Name = "GeoCoordinateForm";
             this.Text = "GeoCoordinateForm";
+            this.Load += new System.EventHandler(this.GeoCoordinateForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,5 +143,7 @@
         private System.Windows.Forms.TextBox edit_UTM;
         private System.Windows.Forms.Label lbl_LatLong;
         private System.Windows.Forms.TextBox edit_LatLong;
+        private System.Windows.Forms.ComboBox cbx_datum;
+        private System.Windows.Forms.Label lbl_datum;
     }
 }
