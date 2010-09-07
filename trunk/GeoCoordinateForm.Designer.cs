650f574ee34361a2be58c78d9bea12594db241f7
ahoerstemeier <ahoerstemeier@1281d315-5448-0410-8fcd-5bcd7f7bbbc7>
2010-09-07 16:39:16 +0000

    GeoHash (completed)

--- trunk/GeoCoordinateForm.Designer.cs
+++ trunk/GeoCoordinateForm.Designer.cs
@@ -29,13 +29,15 @@
         private void InitializeComponent()
         {
             this.lbl_MGRS = new System.Windows.Forms.Label();
-            this.edit_MGRS = new System.Windows.Forms.TextBox();
+            this.edt_MGRS = new System.Windows.Forms.TextBox();
             this.lbl_UTM = new System.Windows.Forms.Label();
-            this.edit_UTM = new System.Windows.Forms.TextBox();
+            this.edt_UTM = new System.Windows.Forms.TextBox();
             this.lbl_LatLong = new System.Windows.Forms.Label();
-            this.edit_LatLong = new System.Windows.Forms.TextBox();
+            this.edt_LatLong = new System.Windows.Forms.TextBox();
             this.cbx_datum = new System.Windows.Forms.ComboBox();
             this.lbl_datum = new System.Windows.Forms.Label();
+            this.lbl_geohash = new System.Windows.Forms.Label();
+            this.edt_geohash = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lbl_MGRS
@@ -47,15 +49,15 @@
             this.lbl_MGRS.TabIndex = 0;
             this.lbl_MGRS.Text = "Thai MGRS";
             // 
-            // edit_MGRS
+            // edt_MGRS
             // 
-            this.edit_MGRS.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            this.edt_MGRS.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.edit_MGRS.Location = new System.Drawing.Point(94, 6);
-            this.edit_MGRS.Name = "edit_MGRS";
-            this.edit_MGRS.Size = new System.Drawing.Size(186, 20);
-            this.edit_MGRS.TabIndex = 1;
-            this.edit_MGRS.TextChanged += new System.EventHandler(this.edit_MGRS_TextChanged);
+            this.edt_MGRS.Location = new System.Drawing.Point(94, 6);
+            this.edt_MGRS.Name = "edt_MGRS";
+            this.edt_MGRS.Size = new System.Drawing.Size(186, 20);
+            this.edt_MGRS.TabIndex = 1;
+            this.edt_MGRS.TextChanged += new System.EventHandler(this.edit_MGRS_TextChanged);
             // 
             // lbl_UTM
             // 
@@ -66,33 +68,33 @@
             this.lbl_UTM.TabIndex = 2;
             this.lbl_UTM.Text = "UTM";
             // 
-            // edit_UTM
+            // edt_UTM
             // 
-            this.edit_UTM.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            this.edt_UTM.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.edit_UTM.Location = new System.Drawing.Point(94, 31);
-            this.edit_UTM.Name = "edit_UTM";
-            this.edit_UTM.Size = new System.Drawing.Size(186, 20);
-            this.edit_UTM.TabIndex = 3;
-            this.edit_UTM.TextChanged += new System.EventHandler(this.edit_UTM_TextChanged);
+            this.edt_UTM.Location = new System.Drawing.Point(94, 31);
+            this.edt_UTM.Name = "edt_UTM";
+            this.edt_UTM.Size = new System.Drawing.Size(186, 20);
+            this.edt_UTM.TabIndex = 3;
+            this.edt_UTM.TextChanged += new System.EventHandler(this.edit_UTM_TextChanged);
             // 
             // lbl_LatLong
             // 
             this.lbl_LatLong.AutoSize = true;
-            this.lbl_LatLong.Location = new System.Drawing.Point(12, 60);
+            this.lbl_LatLong.Location = new System.Drawing.Point(12, 84);
             this.lbl_LatLong.Name = "lbl_LatLong";
             this.lbl_LatLong.Size = new System.Drawing.Size(51, 13);
             this.lbl_LatLong.TabIndex = 4;
             this.lbl_LatLong.Text = "Lat/Long";
             // 
-            // edit_LatLong
+            // edt_LatLong
             // 
-            this.edit_LatLong.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            this.edt_LatLong.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.edit_LatLong.Location = new System.Drawing.Point(94, 57);
-            this.edit_LatLong.Name = "edit_LatLong";
-            this.edit_LatLong.Size = new System.Drawing.Size(186, 20);
-            this.edit_LatLong.TabIndex = 5;
+            this.edt_LatLong.Location = new System.Drawing.Point(94, 81);
+            this.edt_LatLong.Name = "edt_LatLong";
+            this.edt_LatLong.Size = new System.Drawing.Size(186, 20);
+            this.edt_LatLong.TabIndex = 5;
             // 
             // cbx_datum
             // 
@@ -100,7 +102,7 @@
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.cbx_datum.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbx_datum.FormattingEnabled = true;
-            this.cbx_datum.Location = new System.Drawing.Point(94, 83);
+            this.cbx_datum.Location = new System.Drawing.Point(94, 106);
             this.cbx_datum.Name = "cbx_datum";
             this.cbx_datum.Size = new System.Drawing.Size(186, 21);
             this.cbx_datum.TabIndex = 6;
@@ -109,24 +111,43 @@
             // lbl_datum
             // 
             this.lbl_datum.AutoSize = true;
-            this.lbl_datum.Location = new System.Drawing.Point(12, 86);
+            this.lbl_datum.Location = new System.Drawing.Point(12, 109);
             this.lbl_datum.Name = "lbl_datum";
             this.lbl_datum.Size = new System.Drawing.Size(38, 13);
             this.lbl_datum.TabIndex = 7;
             this.lbl_datum.Text = "Datum";
+            // 
+            // lbl_geohash
+            // 
+            this.lbl_geohash.AutoSize = true;
+            this.lbl_geohash.Location = new System.Drawing.Point(12, 59);
+            this.lbl_geohash.Name = "lbl_geohash";
+            this.lbl_geohash.Size = new System.Drawing.Size(50, 13);
+            this.lbl_geohash.TabIndex = 8;
+            this.lbl_geohash.Text = "Geohash";
+            // 
+            // edt_geohash
+            // 
+            this.edt_geohash.Location = new System.Drawing.Point(94, 56);
+            this.edt_geohash.Name = "edt_geohash";
+            this.edt_geohash.Size = new System.Drawing.Size(186, 20);
+            this.edt_geohash.TabIndex = 9;
+            this.edt_geohash.TextChanged += new System.EventHandler(this.edt_geohash_TextChanged);
             // 
             // GeoCoordinateForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.edt_geohash);
+            this.Controls.Add(this.lbl_geohash);
             this.Controls.Add(this.lbl_datum);
             this.Controls.Add(this.cbx_datum);
-            this.Controls.Add(this.edit_LatLong);
+            this.Controls.Add(this.edt_LatLong);
             this.Controls.Add(this.lbl_LatLong);
-            this.Controls.Add(this.edit_UTM);
+            this.Controls.Add(this.edt_UTM);
             this.Controls.Add(this.lbl_UTM);
-            this.Controls.Add(this.edit_MGRS);
+            this.Controls.Add(this.edt_MGRS);
             this.Controls.Add(this.lbl_MGRS);
             this.Name = "GeoCoordinateForm";
             this.Text = "GeoCoordinateForm";
@@ -139,12 +160,14 @@
         #endregion
 
         private System.Windows.Forms.Label lbl_MGRS;
-        private System.Windows.Forms.TextBox edit_MGRS;
+        private System.Windows.Forms.TextBox edt_MGRS;
         private System.Windows.Forms.Label lbl_UTM;
-        private System.Windows.Forms.TextBox edit_UTM;
+        private System.Windows.Forms.TextBox edt_UTM;
         private System.Windows.Forms.Label lbl_LatLong;
-        private System.Windows.Forms.TextBox edit_LatLong;
+        private System.Windows.Forms.TextBox edt_LatLong;
         private System.Windows.Forms.ComboBox cbx_datum;
         private System.Windows.Forms.Label lbl_datum;
+        private System.Windows.Forms.Label lbl_geohash;
+        private System.Windows.Forms.TextBox edt_geohash;
     }
 }
--- trunk/GeoCoordinateForm.cs
+++ trunk/GeoCoordinateForm.cs
@@ -52,33 +52,6 @@ namespace De.AHoerstemeier.Tambon
             return lResult;
         }
 
-        private void edit_MGRS_TextChanged(object sender, EventArgs e)
-        {
-            if (!mChanging)
-            {
-                String lValue = TambonHelper.ReplaceThaiNumerals(edit_MGRS.Text.ToUpper()).Trim();
-                try
-                {
-                    mChanging = true;
-                    if (!TambonHelper.IsNumeric(lValue.Substring(0,2)))
-                    {
-                      lValue = ZoneForThailandMGRS(lValue) + lValue;
-                    }
-                    UTMPoint lUTM = UTMPoint.ParseMGRSString(lValue);
-                    edit_UTM.Text = lUTM.ToString();
-                    GeoPoint lGeo = new GeoPoint(lUTM, (GeoDatum)cbx_datum.SelectedItem);
-                    lGeo.Datum = GeoDatum.DatumWGS84();
-                    edit_LatLong.Text = lGeo.ToString();
-                }
-                catch
-                {
-                    // invalid string
-                    edit_UTM.Text = String.Empty;
-                    edit_LatLong.Text = String.Empty;
-                }
-                mChanging = false;
-            }
-        }
         private void GeoCoordinateForm_Load(object sender, EventArgs e)
         {
             cbx_datum.Items.Add(GeoDatum.DatumWGS84());
@@ -87,28 +60,133 @@ namespace De.AHoerstemeier.Tambon
             cbx_datum.Items.Add(GeoDatum.DatumIndian1954());
         }
 
-        private void edit_UTM_TextChanged(object sender, EventArgs e)
+        private void SetValues(GeoPoint iGeoPoint, UTMPoint iUTM, object sender)
+        {
+            if (sender != edt_LatLong)
+            {
+                if (iGeoPoint == null)
+                {
+                    edt_LatLong.Text = String.Empty;
+                }
+                else
+                {
+                    edt_LatLong.Text = iGeoPoint.ToString();
+                }
+            }
+            if (sender != edt_geohash)
+            {
+                if (iGeoPoint == null)
+                {
+                    edt_geohash.Text = String.Empty;
+                }
+                else
+                {
+                    edt_geohash.Text = iGeoPoint.GeoHash;
+                }
+            }
+            if (sender != edt_UTM)
+            {
+                if (iUTM == null)
+                {
+                    edt_UTM.Text = String.Empty;
+                }
+                else
+                {
+                    edt_UTM.Text = iUTM.ToString();
+                }
+            }
+            if (sender != edt_MGRS)
+            {
+                if (iUTM == null)
+                {
+                    edt_MGRS.Text = String.Empty;
+                }
+                else
+                {
+                    edt_MGRS.Text = iUTM.ToMGRSString(6);
+                }
+            }
+        }
+
+        private void edit_MGRS_TextChanged(object sender, EventArgs e)
         {
             if (!mChanging)
             {
-                String lValue = TambonHelper.ReplaceThaiNumerals(edit_UTM.Text.ToUpper()).Trim();
+                String lValue = TambonHelper.ReplaceThaiNumerals(edt_MGRS.Text.ToUpper()).Trim();
+                GeoPoint lGeo = null;
+                UTMPoint lUTM = null;
                 try
                 {
                     mChanging = true;
-                    UTMPoint lUTM = UTMPoint.ParseUTMString(lValue);
-                    edit_MGRS.Text = lUTM.ToMGRSString(6);
-                    GeoPoint lGeo = new GeoPoint(lUTM, (GeoDatum)cbx_datum.SelectedItem);
+                    if (!TambonHelper.IsNumeric(lValue.Substring(0,2)))
+                    {
+                      lValue = ZoneForThailandMGRS(lValue) + lValue;
+                    }
+                    lUTM = UTMPoint.ParseMGRSString(lValue);
+                    lGeo = new GeoPoint(lUTM, (GeoDatum)cbx_datum.SelectedItem);
                     lGeo.Datum = GeoDatum.DatumWGS84();
-                    edit_LatLong.Text = lGeo.ToString();
                 }
                 catch
                 {
                     // invalid string
-                    edit_MGRS.Text = String.Empty;
-                    edit_LatLong.Text = String.Empty;
+                    lUTM = null;
+                    lGeo = null;
                 }
+                SetValues(lGeo, lUTM, sender);
                 mChanging = false;
             }
+        }
+
+        private void edit_UTM_TextChanged(object sender, EventArgs e)
+        {
+            if (!mChanging)
+            {
+                String lValue = TambonHelper.ReplaceThaiNumerals(edt_UTM.Text.ToUpper()).Trim();
+                GeoPoint lGeo = null;
+                UTMPoint lUTM = null;
+                try
+                {
+                    mChanging = true;
+                    lUTM = UTMPoint.ParseUTMString(lValue);
+                    lGeo = new GeoPoint(lUTM, (GeoDatum)cbx_datum.SelectedItem);
+                    lGeo.Datum = GeoDatum.DatumWGS84();
+                }
+                catch
+                {
+                    // invalid string
+                    lUTM = null;
+                    lGeo = null;
+                }
+                SetValues(lGeo, lUTM, sender);
+                mChanging = false;
+            }
+        }
+
+        private void edt_geohash_TextChanged(object sender, EventArgs e)
+        {
+            if (!mChanging)
+            {
+                GeoPoint lGeo = null;
+                UTMPoint lUTM = null;
+                try
+                {
+                    mChanging = true;
+                    lGeo = new GeoPoint();
+                    lGeo.GeoHash = edt_geohash.Text;
+                    GeoPoint lGeoOtherDatum = new GeoPoint(lGeo);
+                    lGeoOtherDatum.Datum = (GeoDatum)cbx_datum.SelectedItem;
+                    lUTM = lGeoOtherDatum.CalcUTM();
+                }
+                catch
+                {
+                    // invalid string
+                    lUTM = null;
+                    lGeo = null;
+                }
+                SetValues(lGeo, lUTM, sender);
+                mChanging = false;
+            }
+
         }
     }
 }
