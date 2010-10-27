e9fa9573ffc40717f096f143da4a1d1ba2cbb4e3
ahoerstemeier <ahoerstemeier@1281d315-5448-0410-8fcd-5bcd7f7bbbc7>
2010-10-27 17:03:29 +0000

    started asp.net website to display data

---
+++ trunk/Website/AmphoeDataDisplay.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="AmphoeDataDisplay.aspx.cs" Inherits="AmphoeDataDisplay" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    Changwat
+        <asp:DropDownList ID="cbx_changwat" runat="server" AutoPostBack="True" 
+            Height="25px" Width="172px" 
+            onselectedindexchanged="cbx_changwat_SelectedIndexChanged">
+        </asp:DropDownList>
+        <br />
+        Amphoe
+        <asp:DropDownList ID="cbx_amphoe" runat="server" AutoPostBack="True" 
+            onselectedindexchanged="cbx_amphoe_SelectedIndexChanged" Height="16px" 
+            Width="171px">
+        </asp:DropDownList>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ trunk/Website/AmphoeDataDisplay.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+using De.AHoerstemeier.Tambon;
+
+public partial class AmphoeDataDisplay : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            TambonHelper.BaseXMLDirectory = Request.PhysicalApplicationPath + "\\App_Data";
+            FillChangwatCombobox();
+        }
+    }
+
+    protected void FillChangwatCombobox()
+    {
+        if (TambonHelper.Geocodes == null)
+        {
+            TambonHelper.LoadGeocodeList();
+        }
+
+        cbx_changwat.Items.Clear();
+        foreach (PopulationDataEntry lEntry in TambonHelper.Geocodes)
+        {
+            cbx_changwat.Items.Add(new ListItem(lEntry.English,lEntry.Geocode.ToString()));
+        }
+    }
+
+    protected void cbx_amphoe_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        
+    }
+    protected void cbx_changwat_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DropDownList lSender = sender as DropDownList;
+        Int32 lGeocode = Convert.ToInt32(lSender.SelectedValue);
+        String lFilename = TambonHelper.GeocodeSourceFile(lGeocode);
+        PopulationData lGeocodes = null;
+        cbx_amphoe.Items.Clear();
+        if (File.Exists(lFilename))
+        {
+            lGeocodes = PopulationData.Load(lFilename);
+            foreach (PopulationDataEntry lEntry in lGeocodes.Data.SubEntities)
+            {
+                if (!lEntry.Obsolete)
+                {
+                    cbx_amphoe.Items.Add(new ListItem(lEntry.English, lEntry.Geocode.ToString()));
+                }
+            }
+        }
+
+    }
+}
