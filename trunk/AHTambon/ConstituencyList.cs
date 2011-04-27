938c118b67b11e1295ca893019c753f6b8692b5a
ahoerstemeier <ahoerstemeier@1281d315-5448-0410-8fcd-5bcd7f7bbbc7>
2011-04-27 22:29:16 +0000

    Started constituency calculator

---
+++ trunk/AHTambon/ConstituencyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace De.AHoerstemeier.Tambon
+{
+    class ConstituencyList:List<ConstituencyEntry>
+    {
+        public Int32 Population()
+        {
+            Int32 lResult = 0;
+            foreach (ConstituencyEntry lEntry in this)
+            {
+                lResult += lEntry.Population();
+            }
+            return lResult;
+        }
+        public Int32 NumberOfSeats()
+        {
+            Int32 lResult = 0;
+            foreach (ConstituencyEntry lEntry in this)
+            {
+                lResult += lEntry.NumberOfSeats;
+            }
+            return lResult;
+        }
+    }
+}
