c0ceefea5aeda281263f5579cc7a2089fbe01e3b
ahoerstemeier <ahoerstemeier@gmail.com>
2012-06-22 15:01:37 +0000

    Little bit of XML documentation

--- AHTambon/IGeocode.cs
+++ AHTambon/IGeocode.cs
@@ -5,8 +5,18 @@ using System.Text;
 
 namespace De.AHoerstemeier.Tambon
 {
+    /// <summary>
+    /// Interface to handle TIS1099 geocodes.
+    /// </summary>
     interface IGeocode
     {
-        Boolean IsAboutGeocode(Int32 iGeocode, Boolean iIncludeSubEntities);
+        /// <summary>
+        /// Checks if this instance is about the entity identified by the <paramref name="geocode"/>.
+        /// If <paramref name="includeSubEntities"/> is <c>true</c>, 
+        /// </summary>
+        /// <param name="geocode">Geocode to check.</param>
+        /// <param name="includeSubEntities">Toggles whether codes under <paramref name="geocode"/> are considered fitting as well.</param>
+        /// <returns><c>true</c> if instance is about the code, <c>false</c> otherwise.</returns>
+        Boolean IsAboutGeocode(Int32 geocode, Boolean includeSubEntities);
     }
 }
