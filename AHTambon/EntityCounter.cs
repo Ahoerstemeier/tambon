4c2d4ea2e3321d2871b063aafb951a4c953a720e
ahoerstemeier <ahoerstemeier@gmail.com>
2013-07-02 13:27:48 +0000

    replace manual unique with LINQ - faster and more stable

--- AHTambon/EntityCounter.cs
+++ AHTambon/EntityCounter.cs
@@ -2,27 +2,46 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.Linq;
 
 namespace De.AHoerstemeier.Tambon
 {
     public class EntityCounter
     {
         #region properties
+
         private Dictionary<String, Int32> mNamesCount = new Dictionary<String, Int32>();
         private List<EntityType> mEntityTypes;
-        public Int32 BaseGeocode { get; set; }
+
+        public Int32 BaseGeocode
+        {
+            get;
+            set;
+        }
+
         private Int32 mNumberOfEntities;
-        public Int32 NumberOfEntities { get { return mNumberOfEntities; } }
-        #endregion
+
+        public Int32 NumberOfEntities
+        {
+            get
+            {
+                return mNumberOfEntities;
+            }
+        }
+
+        #endregion properties
 
         #region constructor
+
         public EntityCounter(List<EntityType> iEntityTypes)
         {
             mEntityTypes = iEntityTypes;
         }
-        #endregion
+
+        #endregion constructor
 
         #region methods
+
         public String CommonNames(Int32 iCutOff)
         {
             StringBuilder lBuilder = new StringBuilder();
@@ -30,22 +49,24 @@ namespace De.AHoerstemeier.Tambon
             lBuilder.AppendLine("Total number: " + NumberOfEntities.ToString());
 
             List<KeyValuePair<String, Int32>> lSorted = new List<KeyValuePair<String, Int32>>();
-            foreach (KeyValuePair<String, Int32> lKeyValuePair in mNamesCount)
+            foreach ( KeyValuePair<String, Int32> lKeyValuePair in mNamesCount )
             {
                 String lName = lKeyValuePair.Key;
                 lSorted.Add(lKeyValuePair);
             }
-            lSorted.Sort(delegate(KeyValuePair<String, Int32> x, KeyValuePair<String, Int32> y) { return y.Value.CompareTo(x.Value); });
+            lSorted.Sort(delegate(KeyValuePair<String, Int32> x, KeyValuePair<String, Int32> y)
+            {
+                return y.Value.CompareTo(x.Value);
+            });
             Int32 lCount = 0;
-            foreach (KeyValuePair<String, Int32> lKeyValuePair in lSorted)
+            foreach ( KeyValuePair<String, Int32> lKeyValuePair in lSorted )
             {
                 lBuilder.AppendLine(lKeyValuePair.Key + " (" + lKeyValuePair.Value.ToString() + ") ");
                 lCount++;
-                if (lCount > iCutOff)
+                if ( lCount > iCutOff )
                 {
                     break;
                 }
-
             }
 
             String RetVal = lBuilder.ToString();
@@ -55,9 +76,9 @@ namespace De.AHoerstemeier.Tambon
         private List<PopulationDataEntry> LoadGeocodeLists()
         {
             var lList = new List<PopulationDataEntry>();
-            foreach (PopulationDataEntry lEntry in TambonHelper.ProvinceGeocodes)
+            foreach ( PopulationDataEntry lEntry in TambonHelper.ProvinceGeocodes )
             {
-                if (TambonHelper.IsBaseGeocode(BaseGeocode, lEntry.Geocode))
+                if ( TambonHelper.IsBaseGeocode(BaseGeocode, lEntry.Geocode) )
                 {
                     PopulationData lEntities = TambonHelper.GetGeocodeList(lEntry.Geocode);
                     lList.AddRange(lEntities.Data.FlatList(mEntityTypes));
@@ -65,61 +86,47 @@ namespace De.AHoerstemeier.Tambon
             }
             return lList;
         }
+
         private List<PopulationDataEntry> NormalizeNames(List<PopulationDataEntry> iList)
         {
             List<PopulationDataEntry> lResult = new List<PopulationDataEntry>();
-            foreach (PopulationDataEntry lEntry in iList)
+            foreach ( PopulationDataEntry lEntry in iList )
             {
-                if (lEntry.Type == EntityType.Muban)
+                if ( lEntry.Type == EntityType.Muban )
                 {
                     lEntry.Name = TambonHelper.StripBanOrChumchon(lEntry.Name);
                 }
-                if ((!lEntry.IsObsolete()) & (TambonHelper.IsBaseGeocode(BaseGeocode, lEntry.Geocode)))
+                if ( (!lEntry.IsObsolete()) & (TambonHelper.IsBaseGeocode(BaseGeocode, lEntry.Geocode)) )
                 {
                     lResult.Add(lEntry);
                 }
             }
-            lResult.Sort(delegate(PopulationDataEntry p1, PopulationDataEntry p2) { return p1.Name.CompareTo(p2.Name); });
+            lResult.Sort(delegate(PopulationDataEntry p1, PopulationDataEntry p2)
+            {
+                return p1.Name.CompareTo(p2.Name);
+            });
             return lResult;
         }
-        private static Dictionary<String, Int32> DoCalculate(List<PopulationDataEntry> iList)
+
+        private static Dictionary<String, Int32> DoCalculate(List<PopulationDataEntry> list)
         {
-            Dictionary<String, Int32> lResult = new Dictionary<String, Int32>();
-            String lLastname = String.Empty;
-            Int32 lCount = 0;
-            foreach (PopulationDataEntry lEntry in iList)
-            {
-                if (lEntry.Name == lLastname)
-                {
-                    lCount++;
-                }
-                else
-                {
-                    if (!String.IsNullOrEmpty(lLastname))
-                    {
-                        lResult.Add(lLastname, lCount + 1);
-                    }
-                    lCount = 0;
-                    lLastname = lEntry.Name;
-                }
-            }
-            if (!String.IsNullOrEmpty(lLastname))
-            {
-                lResult.Add(lLastname, lCount + 1);
-            }
-            return lResult;
+            var result = list.GroupBy(x => x.Name).ToDictionary(grp => grp.Key, grp => grp.Count());
+            return result;
         }
+
         public void Calculate()
         {
             var lList = LoadGeocodeLists();
             Calculate(lList);
         }
+
         public void Calculate(List<PopulationDataEntry> iInputList)
         {
             var lList = NormalizeNames(iInputList);
             mNumberOfEntities = lList.Count;
             mNamesCount = DoCalculate(lList);
         }
-        #endregion
+
+        #endregion methods
     }
 }
