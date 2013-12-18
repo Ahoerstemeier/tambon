391fcf20f84fc07f2df4c86543b5dc04c13b8d0e
ahoerstemeier <ahoerstemeier@gmail.com>
2013-12-18 11:58:57 +0000

    Common enums not created by model

---
+++ TambonHelpers/CommonEnums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace De.AHoerstemeier.Tambon
+{
+    /// <summary>
+    /// Languages supported, especially for access to WikiData.
+    /// </summary>
+    public enum Language
+    {
+        /// <summary>
+        /// Thai (th)
+        /// </summary>
+        Thai,
+
+        /// <summary>
+        /// English (en)
+        /// </summary>
+        English,
+
+        /// <summary>
+        /// German (de)
+        /// </summary>
+        German,
+    }
+}
