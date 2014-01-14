61afcd86a8417c63a521619de49c0ed771a07439
ahoerstemeier <ahoerstemeier@gmail.com>
2014-01-14 15:58:41 +0000

    WikiDataBot

---
+++ TambonMain/WikiDataBot.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wikibase;
+
+namespace De.AHoerstemeier.Tambon
+{
+    public delegate void WikiDataTaskDelegate(IEnumerable<Entity> entities, StringBuilder collisionInfo, Boolean overrideData);
+
+    public class WikiDataTaskInfo
+    {
+        public String Description
+        {
+            get;
+            private set;
+        }
+
+        public WikiDataTaskDelegate Task
+        {
+            get;
+            private set;
+        }
+
+        public WikiDataTaskInfo(String description, WikiDataTaskDelegate task)
+        {
+            Description = description;
+            Task = task;
+        }
+
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+
+    public class WikiDataBot
+    {
+        private Dictionary<Language, String> _languageCode;
+
+        private List<WikiDataTaskInfo> _availableTasks;
+
+        public IEnumerable<WikiDataTaskInfo> AvailableTasks
+        {
+            get
+            {
+                return _availableTasks;
+            }
+        }
+
+        private WikiDataHelper _helper;
+
+        public WikiDataBot(WikiDataHelper helper)
+        {
+            if ( helper == null )
+            {
+                throw new ArgumentNullException("helper");
+            }
+
+            _helper = helper;
+
+            _availableTasks = new List<WikiDataTaskInfo>();
+            WikiDataTaskDelegate setDescriptionEnglish = (IEnumerable<Entity> entities, StringBuilder collisionInfo, Boolean overrideData) => SetDescription(Language.English, entities, collisionInfo, overrideData);
+            WikiDataTaskDelegate setDescriptionThai = (IEnumerable<Entity> entities, StringBuilder collisionInfo, Boolean overrideData) => SetDescription(Language.Thai, entities, collisionInfo, overrideData);
+            _availableTasks.Add(new WikiDataTaskInfo("Set description [en]", setDescriptionEnglish));
+            _availableTasks.Add(new WikiDataTaskInfo("Set description [th]", setDescriptionThai));
+            _availableTasks.Add(new WikiDataTaskInfo("Set country", SetCountry));
+            _availableTasks.Add(new WikiDataTaskInfo("Set is in administrative unit", SetIsInAdministrativeUnit));
+
+            _languageCode = new Dictionary<Language, String>()
+            {
+                {Language.English,"en"},
+                {Language.German,"de"},
+                {Language.Thai,"th"},
+            };
+        }
+
+        public void LogOut()
+        {
+            _helper.Api.logout();
+        }
+
+        private void SetDescription(Language language, IEnumerable<Entity> entities, StringBuilder collisionInfo, Boolean overrideData)
+        {
+            if ( entities == null )
+            {
+                throw new ArgumentNullException("entities");
+            }
+            var languageCode = _languageCode[language];
+
+            foreach ( var entity in entities )
+            {
+                var item = _helper.GetWikiDataItemForEntity(entity);
+                var oldDescription = item.getDescription(languageCode);
+                var newDescription = entity.GetWikiDataDescription(language);
+
+                if ( String.IsNullOrEmpty(oldDescription) )
+                {
+                    item.setDescription(languageCode, newDescription);
+                    item.save(String.Format("Added description [{0}]: {1}", languageCode, newDescription));
+                }
+                else if ( oldDescription != newDescription )
+                {
+                    if ( collisionInfo != null )
+                    {
+                        collisionInfo.AppendFormat("{0}: {1} already has description [{2}] \"{3}\"", item.id, entity.english, languageCode, oldDescription);
+                        collisionInfo.AppendLine();
+                    }
+                    if ( overrideData )
+                    {
+                        item.setDescription(languageCode, newDescription);
+                        item.save(String.Format("Updated description [{0}]: {1}", languageCode, newDescription));
+                    }
+                }
+            }
+        }
+
+        private void SetCountry(IEnumerable<Entity> entities, StringBuilder collisionInfo, Boolean overrideData)
+        {
+            if ( entities == null )
+            {
+                throw new ArgumentNullException("entities");
+            }
+            foreach ( var entity in entities )
+            {
+                var item = _helper.GetWikiDataItemForEntity(entity);
+                var state = _helper.IsInCountryCorrect(item);
+                if ( state == WikiDataState.WrongValue )
+                {
+                    collisionInfo.AppendFormat("{0}: {1} has wrong country", item.id, entity.english);
+                    collisionInfo.AppendLine();
+                }
+                if ( state != WikiDataState.Valid )
+                {
+                    var statement = _helper.SetIsInCountry(item, overrideData);
+                    if ( statement != null )
+                    {
+                        statement.save(_helper.GetClaimSaveEditSummary(statement));
+                    }
+                }
+            }
+        }
+
+        private void SetIsInAdministrativeUnit(IEnumerable<Entity> entities, StringBuilder collisionInfo, Boolean overrideData)
+        {
+            if ( entities == null )
+            {
+                throw new ArgumentNullException("entities");
+            }
+            foreach ( var entity in entities )
+            {
+                var item = _helper.GetWikiDataItemForEntity(entity);
+                var state = _helper.IsInAdministrativeUnitCorrect(item, entity);
+                if ( state == WikiDataState.WrongValue )
+                {
+                    collisionInfo.AppendFormat("{0}: {1} has wrong parent", item.id, entity.english);
+                    collisionInfo.AppendLine();
+                }
+                if ( state != WikiDataState.Valid )
+                {
+                    var statement = _helper.SetIsInAdministrativeUnit(item, entity, overrideData);
+                    if ( statement != null )
+                    {
+                        statement.save(_helper.GetClaimSaveEditSummary(statement));
+                    }
+                }
+            }
+        }
+
+        public Dictionary<String, Int32> CountSiteLinks(IEnumerable<Entity> entities)
+        {
+            var result = new Dictionary<String, Int32>();
+            // Create a new EntityProvider instance and pass the api created above.
+            EntityProvider entityProvider = new EntityProvider(_helper.Api);
+            foreach ( var entity in entities )
+            {
+                // Get an entity by searching for the id
+                var entityById = entityProvider.getEntityFromId(EntityId.newFromPrefixedId(entity.wiki.wikidata));
+                var links = (entityById as Item).getSitelinks();
+                foreach ( var key in links.Keys )
+                {
+                    if ( !result.ContainsKey(key) )
+                        result[key] = 0;
+                    result[key]++;
+                }
+            }
+            return result;
+        }
+    }
+}
--- TambonUI/WikiData.Designer.cs
+++ TambonUI/WikiData.Designer.cs
@@ -30,12 +30,13 @@
         {
             this.btnStat = new System.Windows.Forms.Button();
             this.btnCountInterwiki = new System.Windows.Forms.Button();
-            this.btnTestGet = new System.Windows.Forms.Button();
-            this.btnTestSet = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
             this.chkTypes = new System.Windows.Forms.CheckedListBox();
             this.cbxActivity = new System.Windows.Forms.ComboBox();
             this.edtCollisions = new System.Windows.Forms.TextBox();
+            this.chkOverride = new System.Windows.Forms.CheckBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnStat
@@ -50,6 +51,7 @@
             // 
             // btnCountInterwiki
             // 
+            this.btnCountInterwiki.Enabled = false;
             this.btnCountInterwiki.Location = new System.Drawing.Point(12, 41);
             this.btnCountInterwiki.Name = "btnCountInterwiki";
             this.btnCountInterwiki.Size = new System.Drawing.Size(98, 23);
@@ -58,71 +60,93 @@
             this.btnCountInterwiki.UseVisualStyleBackColor = true;
             this.btnCountInterwiki.Click += new System.EventHandler(this.btnCountInterwiki_Click);
             // 
-            // btnTestGet
+            // btnRun
             // 
-            this.btnTestGet.Location = new System.Drawing.Point(12, 70);
-            this.btnTestGet.Name = "btnTestGet";
-            this.btnTestGet.Size = new System.Drawing.Size(98, 23);
-            this.btnTestGet.TabIndex = 2;
-            this.btnTestGet.Text = "Test Get";
-            this.btnTestGet.UseVisualStyleBackColor = true;
-            this.btnTestGet.Click += new System.EventHandler(this.btnTestGet_Click);
-            // 
-            // btnTestSet
-            // 
-            this.btnTestSet.Location = new System.Drawing.Point(12, 99);
-            this.btnTestSet.Name = "btnTestSet";
-            this.btnTestSet.Size = new System.Drawing.Size(98, 23);
-            this.btnTestSet.TabIndex = 3;
-            this.btnTestSet.Text = "Test Set";
-            this.btnTestSet.UseVisualStyleBackColor = true;
-            this.btnTestSet.Click += new System.EventHandler(this.btnTestSet_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(116, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Run";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnRun.Enabled = false;
+            this.btnRun.Location = new System.Drawing.Point(124, 41);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(92, 23);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
             // 
             // chkTypes
             // 
+            this.chkTypes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.chkTypes.FormattingEnabled = true;
-            this.chkTypes.Location = new System.Drawing.Point(222, 41);
+            this.chkTypes.Location = new System.Drawing.Point(222, 64);
             this.chkTypes.Name = "chkTypes";
-            this.chkTypes.Size = new System.Drawing.Size(120, 94);
+            this.chkTypes.Size = new System.Drawing.Size(348, 94);
             this.chkTypes.TabIndex = 5;
             // 
             // cbxActivity
             // 
+            this.cbxActivity.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.cbxActivity.FormattingEnabled = true;
             this.cbxActivity.Location = new System.Drawing.Point(222, 14);
             this.cbxActivity.Name = "cbxActivity";
-            this.cbxActivity.Size = new System.Drawing.Size(120, 21);
+            this.cbxActivity.Size = new System.Drawing.Size(348, 21);
             this.cbxActivity.TabIndex = 6;
             // 
             // edtCollisions
             // 
-            this.edtCollisions.Location = new System.Drawing.Point(222, 141);
+            this.edtCollisions.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.edtCollisions.Location = new System.Drawing.Point(222, 164);
             this.edtCollisions.Multiline = true;
             this.edtCollisions.Name = "edtCollisions";
-            this.edtCollisions.Size = new System.Drawing.Size(120, 108);
+            this.edtCollisions.Size = new System.Drawing.Size(348, 258);
             this.edtCollisions.TabIndex = 7;
+            // 
+            // chkOverride
+            // 
+            this.chkOverride.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkOverride.AutoSize = true;
+            this.chkOverride.Location = new System.Drawing.Point(222, 41);
+            this.chkOverride.Name = "chkOverride";
+            this.chkOverride.Size = new System.Drawing.Size(66, 17);
+            this.chkOverride.TabIndex = 8;
+            this.chkOverride.Text = "Override";
+            this.chkOverride.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(124, 12);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(92, 23);
+            this.btnLogin.TabIndex = 9;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Enabled = false;
+            this.btnLogout.Location = new System.Drawing.Point(124, 70);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(92, 25);
+            this.btnLogout.TabIndex = 10;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
             // WikiData
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(683, 261);
+            this.ClientSize = new System.Drawing.Size(582, 434);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.chkOverride);
             this.Controls.Add(this.edtCollisions);
             this.Controls.Add(this.cbxActivity);
             this.Controls.Add(this.chkTypes);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.btnTestSet);
-            this.Controls.Add(this.btnTestGet);
+            this.Controls.Add(this.btnRun);
             this.Controls.Add(this.btnCountInterwiki);
             this.Controls.Add(this.btnStat);
             this.Name = "WikiData";
@@ -137,11 +161,12 @@
 
         private System.Windows.Forms.Button btnStat;
         private System.Windows.Forms.Button btnCountInterwiki;
-        private System.Windows.Forms.Button btnTestGet;
-        private System.Windows.Forms.Button btnTestSet;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRun;
         private System.Windows.Forms.CheckedListBox chkTypes;
         private System.Windows.Forms.ComboBox cbxActivity;
         private System.Windows.Forms.TextBox edtCollisions;
+        private System.Windows.Forms.CheckBox chkOverride;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnLogout;
     }
 }
