3b0341296e931e750b5b187eabf21ad8acded5ac
ahoerstemeier <ahoerstemeier@1281d315-5448-0410-8fcd-5bcd7f7bbbc7>
2014-04-17 16:10:13 +0000

    Avoiding WikiDataBot to run on all items

---
+++ trunk/TambonMain/WikiLocation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace De.AHoerstemeier.Tambon
+{
+    partial class WikiLocation
+    {
+        public Int32 NumericalWikiData
+        {
+            get
+            {
+                Int32 value = -1;
+                if ( !String.IsNullOrWhiteSpace(wikidata) )
+                {
+                    try
+                    {
+                        var subString = wikidata.Remove(0, 1);
+                        value = Convert.ToInt32(subString);
+                    }
+                    catch ( FormatException )
+                    {
+                    }
+                }
+                return value;
+            }
+        }
+    }
+}
