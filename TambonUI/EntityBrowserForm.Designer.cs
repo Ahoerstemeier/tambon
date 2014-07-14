bcfb15d6828bdecaed200572a871acb7f44968d9
ahoerstemeier <ahoerstemeier@gmail.com>
2014-07-14 06:07:00 +0000

    EntityBrowserForm - added numbers of subdivisions

--- TambonUI/EntityBrowserForm.Designer.cs
+++ TambonUI/EntityBrowserForm.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtSubDivisions = new System.Windows.Forms.TextBox();
             this.listviewCentralAdministration = new System.Windows.Forms.ListView();
             this.columnEnglish = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnThai = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnGeocode = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.treeviewSelection = new System.Windows.Forms.TreeView();
             this.columnPopulation = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.treeviewSelection = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtSubDivisions
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtSubDivisions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(201, 314);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(406, 195);
-            this.textBox1.TabIndex = 13;
+            this.txtSubDivisions.Location = new System.Drawing.Point(201, 314);
+            this.txtSubDivisions.Multiline = true;
+            this.txtSubDivisions.Name = "txtSubDivisions";
+            this.txtSubDivisions.Size = new System.Drawing.Size(406, 195);
+            this.txtSubDivisions.TabIndex = 13;
             // 
             // listviewCentralAdministration
             // 
@@ -78,6 +78,12 @@
             // 
             this.columnGeocode.Text = "Geocode";
             // 
+            // columnPopulation
+            // 
+            this.columnPopulation.Text = "Population";
+            this.columnPopulation.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnPopulation.Width = 71;
+            // 
             // treeviewSelection
             // 
             this.treeviewSelection.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -89,18 +95,12 @@
             this.treeviewSelection.TabIndex = 11;
             this.treeviewSelection.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeviewSelection_AfterSelect);
             // 
-            // columnPopulation
-            // 
-            this.columnPopulation.Text = "Population";
-            this.columnPopulation.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.columnPopulation.Width = 71;
-            // 
             // EntityBrowserForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(612, 515);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtSubDivisions);
             this.Controls.Add(this.listviewCentralAdministration);
             this.Controls.Add(this.treeviewSelection);
             this.Name = "EntityBrowserForm";
@@ -113,7 +113,7 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtSubDivisions;
         private System.Windows.Forms.ListView listviewCentralAdministration;
         private System.Windows.Forms.ColumnHeader columnEnglish;
         private System.Windows.Forms.ColumnHeader columnThai;
