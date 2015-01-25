25ff3d6af0110aaf0a9d9d9959732917aab66204
ahoerstemeier@gmail.com <ahoerstemeier@gmail.com>
2015-01-25 22:43:56 +0000

    Census: Maximum error, reference URL to Wikidata

--- TambonMain/PopulationData.cs
+++ TambonMain/PopulationData.cs
@@ -16,18 +16,18 @@ namespace De.AHoerstemeier.Tambon
 
         public void CalculateTotal()
         {
-            if (!data.Any(x => x.type == PopulationDataType.total))
+            if ( !data.Any(x => x.type == PopulationDataType.total) )
             {
                 var result = new HouseholdDataPoint();
                 result.type = PopulationDataType.total;
-                foreach (var element in this.data.Where(x => x.type == PopulationDataType.municipal || x.type == PopulationDataType.nonmunicipal))
+                foreach ( var element in this.data.Where(x => x.type == PopulationDataType.municipal || x.type == PopulationDataType.nonmunicipal) )
                 {
                     result.female += element.female;
                     result.male += element.male;
                     result.total += element.total;
                     result.households += element.households;
                 }
-                if (result.total > 0)
+                if ( result.total > 0 )
                 {
                     data.Add(result);
                 }
@@ -48,6 +48,11 @@ namespace De.AHoerstemeier.Tambon
             // ToDo: Find duplicate entries with same data.type and merge content
         }
 
+        /// <summary>
+        /// Gets the year.
+        /// </summary>
+        /// <value>The year.</value>
+        /// <remarks>Same as <see cref="year"/>, which has wrong datatype as XSD2Code cannot translate the XSD type year better.</remarks>
         public Int16 Year
         {
             get
@@ -56,34 +61,72 @@ namespace De.AHoerstemeier.Tambon
             }
         }
 
+        /// <summary>
+        /// Checks whether the parts sum up correctly.
+        /// </summary>
+        /// <returns><c>true</c> if all parts sum up correctly, <c>false</c> otherwise.</returns>
         public Boolean Verify()
         {
             Boolean result = true;
 
-            foreach (var entry in data)
+            foreach ( var entry in data )
             {
                 result &= entry.Verify();
             }
 
             var municipal = data.FirstOrDefault(x => x.type == PopulationDataType.municipal);
             var rural = data.FirstOrDefault(x => x.type == PopulationDataType.nonmunicipal);
-            if ((municipal != null) && (rural != null))
-            { result &= TotalPopulation.VerifySum(municipal, rural); }
+            if ( (municipal != null) && (rural != null) )
+            {
+                result &= TotalPopulation.VerifySum(municipal, rural);
+            }
             var collectivehouseholds = data.FirstOrDefault(x => x.type == PopulationDataType.collectivehouseholds);
             var privatehouseholds = data.FirstOrDefault(x => x.type == PopulationDataType.privatehouseholds);
-            if ((collectivehouseholds != null) && (privatehouseholds != null))
-            { result &= TotalPopulation.VerifySum(collectivehouseholds, privatehouseholds); }
+            if ( (collectivehouseholds != null) && (privatehouseholds != null) )
+            {
+                result &= TotalPopulation.VerifySum(collectivehouseholds, privatehouseholds);
+            }
             var agricultural = data.FirstOrDefault(x => x.type == PopulationDataType.agricultural);
             var nonagricultural = data.FirstOrDefault(x => x.type == PopulationDataType.nonagricultural);
-            if ((agricultural != null) && (nonagricultural != null))
-            { result &= TotalPopulation.VerifySum(agricultural, nonagricultural); }
+            if ( (agricultural != null) && (nonagricultural != null) )
+            {
+                result &= TotalPopulation.VerifySum(agricultural, nonagricultural);
+            }
             return result;
+        }
+
+        /// <summary>
+        /// Calculates the maximum deviation of the sum of the partial datapoint.
+        /// </summary>
+        /// <returns>Maximum deviation, <c>0</c> if all sum up correctly.</returns>
+        public Int32 SumError()
+        {
+            Int32 maxError = 0;
+            var municipal = data.FirstOrDefault(x => x.type == PopulationDataType.municipal);
+            var rural = data.FirstOrDefault(x => x.type == PopulationDataType.nonmunicipal);
+            if ( (municipal != null) && (rural != null) )
+            {
+                maxError = Math.Max(maxError, TotalPopulation.SumError(municipal, rural));
+            }
+            var collectivehouseholds = data.FirstOrDefault(x => x.type == PopulationDataType.collectivehouseholds);
+            var privatehouseholds = data.FirstOrDefault(x => x.type == PopulationDataType.privatehouseholds);
+            if ( (collectivehouseholds != null) && (privatehouseholds != null) )
+            {
+                maxError = Math.Max(maxError, TotalPopulation.SumError(collectivehouseholds, privatehouseholds));
+            }
+            var agricultural = data.FirstOrDefault(x => x.type == PopulationDataType.agricultural);
+            var nonagricultural = data.FirstOrDefault(x => x.type == PopulationDataType.nonagricultural);
+            if ( (agricultural != null) && (nonagricultural != null) )
+            {
+                maxError = Math.Max(maxError, TotalPopulation.SumError(agricultural, nonagricultural));
+            }
+            return maxError;
         }
 
         internal void AddDataPoint(HouseholdDataPoint dataPoint)
         {
             var target = data.FirstOrDefault(x => x.type == dataPoint.type);
-            if (target == null)
+            if ( target == null )
             {
                 target = new HouseholdDataPoint();
                 target.type = dataPoint.type;
@@ -113,24 +156,65 @@ namespace De.AHoerstemeier.Tambon
             (this.total == data.total);
         }
 
+        /// <summary>
+        /// Verifies if <see cref="male"/> and <see cref="female"/> sum up to <see cref="total"/>.
+        /// </summary>
+        /// <returns><c>true</c> if valid, <c>false</c> otherwise.</returns>
         public Boolean Verify()
         {
             var sum = male + female;
             return (sum == total);
         }
 
-        public Boolean VerifySum(PopulationDataPoint data1, PopulationDataPoint data2)
+        /// <summary>
+        /// Deviation of sum <see cref="male"/> and <see cref="female"/> with <see cref="total"/>.
+        /// </summary>
+        /// <returns>Deviation of sum, <c>0</c> is sum is valid.</returns>
+        public Int32 SumError()
+        {
+            var sum = male + female;
+            return Math.Abs(sum - total);
+        }
+
+        /// <summary>
+        /// Checks whether the two population data added together are equal with this.
+        /// </summary>
+        /// <param name="data1">First datapoint.</param>
+        /// <param name="data2">Second datapoint.</param>
+        /// <returns>Maximum deviation in one of the sum, <c>0</c> if equal.</returns>
+        public Int32 SumError(PopulationDataPoint data1, PopulationDataPoint data2)
         {
             var sum = new PopulationDataPoint();
-            if (data1 != null)
+            if ( data1 != null )
             {
                 sum.Add(data1);
             }
-            if (data2 != null)
+            if ( data2 != null )
             {
                 sum.Add(data2);
             }
-            return this.IsEqual(sum);
+            return this.MaxDeviation(sum);
+        }
+
+        private Int32 MaxDeviation(PopulationDataPoint sum)
+        {
+            return Math.Max(
+                Math.Abs(this.total - sum.total),
+                Math.Max(
+                    Math.Abs(this.male - sum.male),
+                    Math.Abs(this.female - sum.female))
+                );
+        }
+
+        /// <summary>
+        /// Checks whether the two population data added together are equal with this.
+        /// </summary>
+        /// <param name="data1">First datapoint.</param>
+        /// <param name="data2">Second datapoint.</param>
+        /// <returns><c>true</c> if equal, <c>false</c> otherwise.</returns>
+        public Boolean VerifySum(PopulationDataPoint data1, PopulationDataPoint data2)
+        {
+            return this.SumError(data1, data2) == 0;
         }
     }
 }
