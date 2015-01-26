cd7e4a629c0ca86d314b9e49080cee86b7915f93
ahoerstemeier@googlemail.com <ahoerstemeier@googlemail.com@1281d315-5448-0410-8fcd-5bcd7f7bbbc7>
2015-01-26 18:03:11 +0000

    Wikidatabot: Amphoe slogan reference

---
+++ trunk/TambonMain/VisionSlogan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace De.AHoerstemeier.Tambon
+{
+    public partial class VisionSlogan
+    {
+        /// <summary>
+        /// Gets the <see cref="Value"/> as a single lined string.
+        /// </summary>
+        /// <value>The value as a single lined string.</value>
+        public String SingleLineValue
+        {
+            get
+            {
+                var multiLineSlogan = Value.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim());
+                return String.Join(" ", multiLineSlogan).Trim();
+            }
+        }
+    }
+}
