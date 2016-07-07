3e620aec195cf46cd7f300937ec2079d3683f15d
Andreas Hörstemeier <ahoerstemeier@gmail.com>
2016-07-07 18:55:49 +0200

    Histories

--- DataModel/history.Designer.cs
+++ DataModel/history.Designer.cs
@@ -653,6 +653,14 @@ namespace De.AHoerstemeier.Tambon {
         
         private List<uint> changewithField;
         
+        private int subdivisionsField;
+        
+        private bool subdivisionsFieldSpecified;
+        
+        private EntityType subdivisiontypeField;
+        
+        private bool subdivisiontypeFieldSpecified;
+        
         /// <summary>
         /// Creates a new instance of HistoryAreaChange.
         /// </summary>
@@ -776,6 +784,74 @@ namespace De.AHoerstemeier.Tambon {
             }
             set {
                 this.changewithField = value;
+            }
+        }
+        
+        /// <summary>
+        /// Auto generated comment tag to suppress XML code documentation warning.
+        /// </summary>
+        /// <value>
+        /// Auto generated value tag to suppress XML code documentation warning.
+        /// </value>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public int subdivisions {
+            get {
+                return this.subdivisionsField;
+            }
+            set {
+                this.subdivisionsField = value;
+            }
+        }
+        
+        /// <summary>
+        /// Auto generated comment tag to suppress XML code documentation warning.
+        /// </summary>
+        /// <value>
+        /// Auto generated value tag to suppress XML code documentation warning.
+        /// </value>
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public bool subdivisionsSpecified {
+            get {
+                return this.subdivisionsFieldSpecified;
+            }
+            set {
+                this.subdivisionsFieldSpecified = value;
+            }
+        }
+        
+        /// <summary>
+        /// Auto generated comment tag to suppress XML code documentation warning.
+        /// </summary>
+        /// <value>
+        /// Auto generated value tag to suppress XML code documentation warning.
+        /// </value>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public EntityType subdivisiontype {
+            get {
+                return this.subdivisiontypeField;
+            }
+            set {
+                this.subdivisiontypeField = value;
+            }
+        }
+        
+        /// <summary>
+        /// Auto generated comment tag to suppress XML code documentation warning.
+        /// </summary>
+        /// <value>
+        /// Auto generated value tag to suppress XML code documentation warning.
+        /// </value>
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public bool subdivisiontypeSpecified {
+            get {
+                return this.subdivisiontypeFieldSpecified;
+            }
+            set {
+                this.subdivisiontypeFieldSpecified = value;
             }
         }
     }
