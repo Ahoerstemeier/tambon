54f23a87f38dfceb6a65492beb0c5f8b0ac1f447
Andreas Hörstemeier <ahoerstemeier@gmail.com>
2021-02-10 20:10:18 +0100

    fix google search

--- TambonUI/EntityBrowserForm.cs
+++ TambonUI/EntityBrowserForm.cs
@@ -1363,7 +1363,7 @@ namespace De.AHoerstemeier.Tambon.UI
                 {
                     var entity = item.Tag as Entity;
 
-                    var url = String.Format(CultureInfo.CurrentUICulture, "https://www.google.de/#q={0}", entity.FullName);
+                    var url = String.Format(CultureInfo.CurrentUICulture, "https://www.google.de/search?source=hp&q={0}", entity.FullName);
                     Process.Start(url);
                 }
             }
